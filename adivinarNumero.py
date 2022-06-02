97d59bc323588a0eca953651c71000dbd9047b35
Julian <jarc2802@gmail.com>
2022-06-01 21:06:52 -0500

    se crea juego para que usuario adivine número random

---
+++ adivinarNumero.py
@@ -0,0 +1,45 @@
+from random import *
+
+intentos=8
+turnosfinal=0
+
+aleatorio=randint(1,101)
+print(aleatorio)
+
+print("Bienvenido existe un numero entre 1 y 100 que debe adivinar en 8 turnos.")
+print("\n")
+
+print(f"Usted tiene {intentos} para adivinar, empecemos:")
+print("********************************************************")
+print("\n")
+
+while intentos <= 8 and intentos >0 :
+
+   
+    numeroUsuario=int(input("Ingrese un numero: "))
+    
+    if numeroUsuario > 0 and numeroUsuario > 100:
+        print ("Recuerde el rango permitido es de 1 - 100")
+        print("\n")
+    elif numeroUsuario > aleatorio:
+            turnosfinal+=1
+            intentos -= 1
+            print ("El numero ingresado es mayor, intente de nuevo")
+            print(f"Quedan {intentos} intentos para adivinar")
+            print("\n")
+    elif numeroUsuario < aleatorio:
+            turnosfinal+=1
+            intentos -= 1
+            print ("El numero ingresado es menor, intente de nuevo")
+            print(f"Quedan {intentos} intentos para adivinar")
+            print("\n")
+    else:
+        print ("Felicitaciones adivinó el número")
+        turnosfinal+=1
+        
+        print(f"Le tomo {turnosfinal} intentos para adivinar")
+        intentos=0
+
+
+
+
