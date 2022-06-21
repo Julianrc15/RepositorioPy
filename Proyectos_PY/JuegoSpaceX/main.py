c49d5c09f32f8227657664a72151ce0410c06416
Julian <jarc2802@gmail.com>
2022-06-21 13:34:49 -0500

    se codifica sonido y efectos  para spacex game

--- Proyectos_PY/JuegoSpaceX/main.py
+++ Proyectos_PY/JuegoSpaceX/main.py
@@ -1,5 +1,7 @@
+import math
 import pygame
 import random
+from pygame import mixer
 
 # inicializar pygame
 pygame.init()
@@ -13,6 +15,11 @@ icono= pygame.image.load("Proyectos_PY\JuegoSpaceX\spaceship.png")
 pygame.display.set_icon(icono)
 fondo= pygame.image.load("Proyectos_PY\JuegoSpaceX\pantalla.png")
 
+#musica de fondo
+mixer.music.load("Proyectos_PY\JuegoSpaceX\MusicaFondo.mp3")
+mixer.music.set_volume(0.3)
+mixer.music.play(-1)
+
 #jugador
 img_jugador=pygame.image.load("Proyectos_PY\JuegoSpaceX\spaceship1.png")
 jugador_x=368
@@ -20,19 +27,39 @@ jugador_y=500
 jugador_x_cambio=0
 
 #enemigo
-img_enemigo=pygame.image.load("Proyectos_PY\JuegoSpaceX\enemigo.png")
-enemigo_x=random.randint(0,736)
-enemigo_y=random.randint(50,200)
-enemigo_x_cambio= 0.1
-enemigo_y_cambio= 50
+img_enemigo=[]
+enemigo_x=[]
+enemigo_y=[]
+enemigo_x_cambio= []
+enemigo_y_cambio= []
+cantidad_enemigos= 8
+
+for e in range(cantidad_enemigos):
+
+    img_enemigo.append(pygame.image.load("Proyectos_PY\JuegoSpaceX\enemigo.png"))
+    enemigo_x.append(random.randint(0,736))
+    enemigo_y.append(random.randint(50,200))
+    enemigo_x_cambio.append( 0.4 )
+    enemigo_y_cambio.append( 50 )
 
 #bala
 img_bala=pygame.image.load("Proyectos_PY\JuegoSpaceX\lasser.png")
 bala_x=0
 bala_y=500
 bala_x_cambio= 0
-bala_y_cambio= 0.7
+bala_y_cambio= 1.5
 bala_visible= False
+
+#puntaje
+puntaje=0
+fuente= pygame.font.Font('freesansbold.ttf', 32)
+texto_x= 10
+texto_y = 10
+
+#función mostrar puntaje
+def mostrar_puntaje(x, y):
+    texto = fuente.render(f"Score: {puntaje}", True, (255, 255, 255))
+    pantalla.blit(texto,(x,y))
 
 
 
@@ -43,14 +70,21 @@ def jugador(x,y):
 
 
 #función enemigo
-def enemigo(x,y):
-    pantalla.blit(img_enemigo, (x,y))
+def enemigo(x,y, enemigo):
+    pantalla.blit(img_enemigo[e], (x,y))
 
 #función disparar_bala
 def disparar_bala(x,y):
     global bala_visible
     bala_visible = True
     pantalla.blit(img_bala, (x + 16, y + 10))
+
+def hay_colision(x1,y1, x2,y2):
+    distancia=math.sqrt(math.pow(x1-x2,2)+ math.pow(y2-y1,2))
+    if distancia < 27:
+        return True
+    else:
+        return False
 
 
 
@@ -74,13 +108,16 @@ while se_ejecuta:
         #presionar teclas
         if evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_LEFT:
-                jugador_x_cambio = -0.5
+                jugador_x_cambio = -1
                 print("izq")
             if evento.key == pygame.K_RIGHT:
-                jugador_x_cambio = 0.5
+                jugador_x_cambio = 1
                 print("der")
             if evento.key == pygame.K_SPACE:
+                sonido_bala= mixer.Sound("Proyectos_PY\JuegoSpaceX\disparo.mp3")
+
                 if not bala_visible:
+                    sonido_bala.play()
                     bala_x= jugador_x
                     disparar_bala(bala_x,bala_y)
                        
@@ -106,15 +143,30 @@ while se_ejecuta:
 
         
     #modificar enemigo
-    enemigo_x += enemigo_x_cambio
+    for e in range(cantidad_enemigos):
+        enemigo_x[e] += enemigo_x_cambio[e]
    
     #mantener dentro de lo bordes al enemigo
-    if enemigo_x <= 0:
-        enemigo_x_cambio = 0.1
-        enemigo_y += enemigo_y_cambio
-    elif enemigo_x >= 736:
-        enemigo_x_cambio= -0.1
-        enemigo_y += enemigo_y_cambio
+        if enemigo_x[e] <= 0:
+            enemigo_x_cambio[e] = 0.4
+            enemigo_y [e]+= enemigo_y_cambio[e]
+        elif enemigo_x [e]>= 736:
+            enemigo_x_cambio[e]= -0.4
+            enemigo_y[e]+= enemigo_y_cambio[e]
+
+        #colision
+        colision =(hay_colision(enemigo_x[e], enemigo_y[e], bala_x, bala_y))
+        if colision:
+            sonido_colision= mixer.Sound("Proyectos_PY\JuegoSpaceX\Golpe.mp3")
+            sonido_colision.play()
+            bala_y= 500
+            bala_visible= False
+            puntaje += 1
+            enemigo_x[e]=random.randint(0,736)
+            enemigo_y[e]=random.randint(50,200)
+            
+        enemigo(enemigo_x[e],  enemigo_y[e], e)
+
 
     #movimiento bala
     if bala_y <= -32:
@@ -125,8 +177,14 @@ while se_ejecuta:
         disparar_bala( bala_x, bala_y )
         bala_y -= bala_y_cambio 
 
+    
+
 
     jugador(jugador_x,jugador_y)
-    enemigo(enemigo_x,  enemigo_y)
+
+    mostrar_puntaje(texto_x,texto_y)
+    
+
+
     #actualizar
     pygame.display.update()
